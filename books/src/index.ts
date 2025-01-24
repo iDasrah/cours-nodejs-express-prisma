54fbf8df0ee94b6ca2a1653f7a5ae6cf4eeaf8e6
dasrah <dasrah.dev@gmail.com>
2025-01-24 14:16:32 +0100

    ajout routes get authors

--- books/src/index.ts
+++ books/src/index.ts
@@ -1,10 +1,28 @@
 import express, { Request, Response, NextFunction } from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
+});
+
+app.get('/authors', async (req: Request, res: Response) => {
+    const authors = await prisma.author.findMany();
+    res.json(authors);
+});
+
+app.get('/authors/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const author = await prisma.author.findUnique({
+        where: {
+            id: parseInt(id)
+        }
+    });
+    res.json(author);
 });
 
 app.listen(port, () => {
