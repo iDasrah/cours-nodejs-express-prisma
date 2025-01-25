e5dc0e371e0e42a38b1ccf2823fccc8c9a20ccea
dasrah <dasrah.dev@gmail.com>
2025-01-25 13:49:02 +0100

    schema validation update author

--- books/src/validation/author.ts
+++ books/src/validation/author.ts
@@ -1,6 +1,11 @@
-import { object, string, size } from 'superstruct';
+import {object, string, size, optional} from 'superstruct';
 
 export const AuthorCreationData = object({
     firstName: size(string(), 1, 50),
     lastName: size(string(), 1, 50),
+});
+
+export const AuthorUpdateData = object({
+    firstName: optional(size(string(), 1, 50)),
+    lastName: optional(size(string(), 1, 50)),
 });
