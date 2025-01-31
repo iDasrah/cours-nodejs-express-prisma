bfd355e2c13b2b5b72d21822927ee70f9ae2b7bd
dasrah <dasrah.dev@gmail.com>
2025-01-31 11:23:33 +0100

    ajout du modèle Tag, migration

--- books/prisma/seed.ts
+++ books/prisma/seed.ts
@@ -2,6 +2,15 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+const tags = [
+    {
+        name: 'Fantasy'
+    },
+    {
+        name: 'Horror'
+    }
+];
+
 const authors = [
     {
         firstName: 'J. R. R.',
@@ -11,10 +20,20 @@ const authors = [
                 {
                     title: 'The Hobbit',
                     publication_year: 1937,
+                    tags: {
+                        connect: {
+                            name: 'Fantasy'
+                        }
+                    }
                 },
                 {
                     title: 'The Lord of the Rings',
                     publication_year: 1954,
+                    tags: {
+                        connect: {
+                            name: 'Fantasy'
+                        }
+                    }
                 }
             ]
         }
@@ -25,12 +44,22 @@ const authors = [
         books: {
             create: [
                 {
-                    title: 'At the Mountains of Madness',
-                    publication_year: 1936,
-                },
-                {
                     title: 'The Call of Cthulhu',
                     publication_year: 1928,
+                    tags: {
+                        connect: {
+                            name: 'Horror'
+                        }
+                    }
+                },
+                {
+                    title: 'At the Mountains of Madness',
+                    publication_year: 1936,
+                    tags: {
+                        connect: {
+                            name: 'Horror'
+                        }
+                    }
                 }
             ]
         }
@@ -38,6 +67,11 @@ const authors = [
 ];
 
 async function main() {
+    for (const tag of tags) {
+        await prisma.tag.create({
+            data: tag,
+        });
+    }
     for (const author of authors) {
         await prisma.author.create({
             data: author,
